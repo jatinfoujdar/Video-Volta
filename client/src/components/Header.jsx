5960a70cd6099b5a37a0557bce890312bb98c060
Jatin Foujdar <jatin321001@gmail.com>
2024-06-06 21:00:35 +0530

    constant logo commit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,6 +5,7 @@ import {useNavigate} from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { addUser, removeUser } from '../util/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
+import { LOGO } from '../util/constant';
 
 
 
@@ -22,7 +23,7 @@ const Header = () => {
 });
 }
 useEffect(()=>{
-  onAuthStateChanged(auth, (user) => {
+  const unsubscribe  = onAuthStateChanged(auth, (user) => {
     if (user) {
       // User is signed in, see docs for a list of available properties
       // https://firebase.google.com/docs/reference/js/auth.user
@@ -35,12 +36,13 @@ useEffect(()=>{
       dispatch(removeUser())
      navigate("/");
     }
-  });
+  })
+  return ()=> unsubscribe();
 },[])
   return (
     <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
         <img className='w-44'
-         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="jumpman" /> 
+         src= {LOGO} alt="jumpman" /> 
    
    {user && (<div className='flex p-2'>
     <img className='w-12 h-12 ' alt='usericon'
