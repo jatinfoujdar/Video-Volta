4ca54f2b7efc4faadd93be7f91c1f3a9bc6c883a
Jatin Foujdar <jatin321001@gmail.com>
2024-06-14 04:49:47 +0530

    title description commit

---
+++ client/src/components/VideoTitle.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const VideoTitle = ({title,overview}) => {
+  return (
+    <div className='pt-36 px-12'>
+        <h1 className='font-bold text-6xl'>{title}</h1>
+        <p className='py-6 text-lg w-1/4 '>{overview}</p>
+        <div>
+            <button className='text-black bg-gray-500 font-bold py-2 px-4 rounded bg-opacity-30' > ▶ Play</button>
+            <button className='text-black bg-gray-500 font-bold py-2 px-4 rounded bg-opacity-30' >More Info</button>
+        </div>
+    </div>
+  )
+}
+
+export default VideoTitle
