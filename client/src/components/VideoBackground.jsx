a2992d7cdadb75b7ddd15966c975de8ae6a278d1
Jatin Foujdar <jatin321001@gmail.com>
2024-06-15 16:43:13 +0530

    filter data commit

--- client/src/components/VideoBackground.jsx
+++ client/src/components/VideoBackground.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { API_OPTION } from '../util/constant'
 
 const VideoBackground = ({movieId}) => {
@@ -7,8 +7,13 @@ const VideoBackground = ({movieId}) => {
     const data = await fetch('https://api.themoviedb.org/3/movie/976573/videos?language=en-US', API_OPTION);
     const json = await data.json();
     console.log(json);
+    const filterData = json.results.filter((video) => video.type === "Trailer")
+    const trailer = filterData.length  ? data[0] : json.results[0] ;
+    console.log(trailer);
   }
-
+  useEffect(()=>{
+    getMovieVideos();
+  },[]);
   return (
     <div>VideoBackground</div>
   )
