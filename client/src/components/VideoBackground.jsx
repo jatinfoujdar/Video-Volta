f152c5399d37343258d2eb59ae01255ccc168712
Jatin Foujdar <jatin321001@gmail.com>
2024-06-15 17:33:03 +0530

    restructure commit

--- client/src/components/VideoBackground.jsx
+++ client/src/components/VideoBackground.jsx
@@ -1,22 +1,45 @@
-import React, { useEffect } from 'react'
-import { API_OPTION } from '../util/constant'
+import React, { useEffect, useState } from 'react';
+import { API_OPTION } from '../util/constant';
 
-const VideoBackground = ({movieId}) => {
+const VideoBackground = ({ movieId }) => {
+  const [trailer, setTrailer] = useState(null);
 
-  const getMovieVideos = async()=>{
-    const data = await fetch('https://api.themoviedb.org/3/movie/976573/videos?language=en-US', API_OPTION);
-    const json = await data.json();
-    console.log(json);
-    const filterData = json.results.filter((video) => video.type === "Trailer")
-    const trailer = filterData.length  ? data[0] : json.results[0] ;
-    console.log(trailer);
-  }
-  useEffect(()=>{
+  const getMovieVideos = async () => {
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`, API_OPTION);
+      if (!response.ok) {
+        throw new Error('Failed to fetch trailer');
+      }
+      const json = await response.json();
+      console.log(json);
+      const filteredData = json.results.filter((video) => video.type === "Trailer");
+      const selectedTrailer = filteredData.length ? filteredData[0] : json.results[0];
+      setTrailer(selectedTrailer);
+      console.log(selectedTrailer);
+    } catch (error) {
+      console.error('Error fetching trailer:', error);
+    }
+  };
+
+  useEffect(() => {
     getMovieVideos();
-  },[]);
-  return (
-    <div>VideoBackground</div>
-  )
-}
+  }, []); // Run effect whenever movieId changes
 
-export default VideoBackground
+  return (
+    <div>
+      {trailer ? (
+        <iframe
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${trailer.key}`}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        ></iframe>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default VideoBackground;
